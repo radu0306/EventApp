49fbb1071fc7572464e30a6ad10beb0bab252dc2
RaduA <radu_a_03@yahoo.com>
2018-01-11 17:50:17 +0200

    First commit EventApp with setup completed succesfully

---
+++ Backend/src/main/java/net/radu/Backend/App.java
@@ -0,0 +1,13 @@
+package net.radu.Backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
